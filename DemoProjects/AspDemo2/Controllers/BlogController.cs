0868fce4e39ae01fa0eb7d36c6f6d9d0ae14e479
davidpue <d.pue@gmx-topmail.de>
2015-10-18 16:01:23 +0200

    asp .net 5 project added

---
+++ DemoProjects/AspDemo2/Controllers/BlogController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using EF7Demo;
+
+namespace AspDemo2.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogController : Controller
+    {
+        private static int _i;
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Blog> Get()
+        {
+            var ctx = new BloggingContext();
+            var blog = new Blog
+            {
+                Url = "Testurl" + (++ _i)
+            };
+            ctx.Add(blog);
+
+            return ctx.Blogs.Take(3);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
